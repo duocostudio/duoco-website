dc1d07c20834cb179d96c67179b41a11d19d58ee
Andres Michel <andres.michel@scalablepress.com>
2020-09-16 22:29:04 -0700

    add check icon

--- components/ServiceItem.js
+++ components/ServiceItem.js
@@ -1,7 +1,11 @@
+import Check from '../public/check.svg'
+
 export default function ServiceItem({ children }) {
   return (
     <li className="flex flex-row items-center mb-8">
-      <div className="bg-gray-400 h-8 w-8 rounded-full"></div>
+      <div className="bg-gray-400 h-8 w-8 rounded-full">
+        <Check />
+      </div>
       <span className="ml-6 text-xl font-semibold">{children}</span>
     </li>
   )
